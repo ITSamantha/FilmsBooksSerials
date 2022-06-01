af5e491055d9931e70dd00b26fd980414dd047d4
ITSamantha <diana.saveleva.dos@gmail.com>
2022-06-01 12:36:57 +0300

    Created GUI without events

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import sys
 
 from mainform import Ui_MainWindow as MyWindow
 
+# Constants
+LOGO_IMAGE_DIRECTORY = "images\\insp.png"
+
 
 class WorkWithApplication(QtWidgets.QMainWindow):
 
@@ -15,14 +18,26 @@ class WorkWithApplication(QtWidgets.QMainWindow):
         self.init_form()
 
     def init_form(self):
-
         self.ui.pages.setCurrentIndex(0)
-        self.setWindowIcon(QIcon("images\\insp.png"))
+        self.setWindowIcon(QIcon(LOGO_IMAGE_DIRECTORY))
+        self.ui.booksButton.clicked.connect(
+            lambda ch, btn=self.ui.booksButton: self.button_clicked(btn.text()))  # If clicked, we get a signal
+        self.ui.filmsButton.clicked.connect(
+            lambda ch, btn=self.ui.filmsButton: self.button_clicked(btn.text()))
+        self.ui.serialsButton.clicked.connect(
+            lambda ch, btn=self.ui.serialsButton: self.button_clicked(btn.text()))
 
+
+    def button_clicked(self, value):
+        if value == "Books":
+            self.ui.pages.setCurrentIndex(2)
+        elif value == "Films":
+            self.ui.pages.setCurrentIndex(3)
+        else:
+            self.ui.pages.setCurrentIndex(1)
 
 
 app = QtWidgets.QApplication([])
 application = WorkWithApplication()
 application.show()
-
-sys.exit(app.exec_())
+sys.exit(app.exec())
